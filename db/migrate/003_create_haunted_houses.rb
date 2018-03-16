0a6438bcff688f2ed3699cc212f38e48ec57a5d5
Eden Mesfun <emesfun@gmail.com>
2018-03-16 17:00:06 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 
-class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
 
   def change
       create_table :hauntedhouses do |type|
