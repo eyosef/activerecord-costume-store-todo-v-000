9191f1dbd14d7869e567103a3eff622c5292cef0
Eden Mesfun <emesfun@gmail.com>
2018-03-16 17:14:40 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |type|
     type.string :name
     type.string :location #CHECK
-    type.integer :location
+    type.integer :location #CHECK
 
     type.string :size
     type.string :url
