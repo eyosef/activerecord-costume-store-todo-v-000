15c078f6a5ff7e39c2b36f58ec1effc18b00f7d5
Eden Mesfun <emesfun@gmail.com>
2018-03-16 16:46:24 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.float "price"
     t.string "size"
-    t.string "url"
+    t.string "image_url"
+    t.datetime "created_at"
   end
 
   create_table "costumestores", force: :cascade do |t|
